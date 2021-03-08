94ef90459f608721d7a345c50607657326c82c2d
clux <sszynrae@gmail.com>
2021-03-08 12:53:52 +0000

    further minimize example

--- blah.rs
+++ blah.rs
@@ -1,12 +1,12 @@
+use tracing_subscriber::layer::SubscriberExt;
+
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 type Result<T> = std::result::Result<T, Error>;
 
-#[tracing::instrument(fields(traceId))]
+#[tracing::instrument]
 async fn trace_test() ->  Result<()> {
     tracing::warn!("start");
-    tracing::Span::current().record("traceId", &tracing::field::display(&get_trace_id()));
-    tracing::warn!("got id");
-    tracing::info!("end! span: {:?}", tracing::Span::current());
+    tracing::warn!("end! span: {:?}", tracing::Span::current());
     Ok(())
 }
 
@@ -18,14 +18,10 @@ async fn main() -> Result<()> {
         .with_endpoint(&otlp_endpoint)
         .install()?;
 
-    // Add a stdout logger as well
-    let logger = tracing_subscriber::fmt::layer();
-
     // Register all subscribers
-    use tracing_subscriber::layer::SubscriberExt;
     let collector = tracing_subscriber::registry()
         .with(tracing_opentelemetry::layer().with_tracer(tracer))
-        .with(logger);
+        .with(tracing_subscriber::fmt::layer());
 
     // Uncomment this for a working example (and remove async from trace_test)
     //tracing::subscriber::with_default(collector, || {
@@ -39,10 +35,4 @@ async fn main() -> Result<()> {
     tracing::info!("trace test returned: {:?}", r);
 
     Ok(())
-}
-
-pub fn get_trace_id() -> String {
-    use tracing_opentelemetry::OpenTelemetrySpanExt; // tracing::Span to opentelemetry::Context
-    use opentelemetry::trace::TraceContextExt; // opentelemetry::Context -> opentelemetry::trace::Span
-    tracing::Span::current().context().span().span_context().trace_id().to_hex()
 }
