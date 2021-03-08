a07e34f22210be8dc4a32eed117792ad5c651b68
clux <sszynrae@gmail.com>
2021-03-08 13:12:22 +0000

    being able to debug if ids show up is pertinent

--- blah.rs
+++ blah.rs
@@ -3,8 +3,9 @@ use tracing_subscriber::layer::SubscriberExt;
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 type Result<T> = std::result::Result<T, Error>;
 
-#[tracing::instrument]
+#[tracing::instrument(fields(traceId))]
 async fn trace_test() ->  Result<()> {
+    tracing::Span::current().record("traceId", &tracing::field::display(&get_trace_id()));
     tracing::warn!("start");
     tracing::warn!("end! span: {:?}", tracing::Span::current());
     Ok(())
@@ -35,4 +36,11 @@ async fn main() -> Result<()> {
     tracing::info!("trace test returned: {:?}", r);
 
     Ok(())
+}
+
+// log helper so we can figure out if the spans ended up on grafana tempo
+fn get_trace_id() -> String {
+    use tracing_opentelemetry::OpenTelemetrySpanExt; // tracing::Span to opentelemetry::Context
+    use opentelemetry::trace::TraceContextExt; // opentelemetry::Context -> opentelemetry::trace::Span
+    tracing::Span::current().context().span().span_context().trace_id().to_hex()
 }
